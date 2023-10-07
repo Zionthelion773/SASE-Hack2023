0a595e91eaf1c3341ede1d8a24362b36f6ab389c
Zion <Zbishop@hawk.iit.edu>
2023-10-07 18:58:39 -0500

    Dylan is gay

--- Potluck/App.js
+++ Potluck/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>fuck you Zion</Text>
+      <Text>fuck you Dylan</Text>
       <StatusBar style="auto" />
     </View>
   );
