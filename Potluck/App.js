bf7b9725e7f0cbd6d5e11cb3a0f50785d7c57db1
Dylan <dputra@hawk.iit.edu>
2023-10-07 18:57:15 -0500

    a

--- Potluck/App.js
+++ Potluck/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello this is Zion</Text>
+      <Text>fuck you Zion</Text>
       <StatusBar style="auto" />
     </View>
   );
