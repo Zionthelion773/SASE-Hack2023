d5068fe882d4bb35e9f6ece2be4572b45bea6a16
Zion <Zbishop@hawk.iit.edu>
2023-10-07 19:20:39 -0500

    potluck commit

--- Potluck/App.js
+++ Potluck/App.js
@@ -1,10 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [dish, setDish] = useState(''); // State for the input
+  const [dishes, setDishes] = useState([]); // State for the list of dishes
+
+  const addDish = () => {
+    if (dish.trim() !== '') {
+      setDishes([...dishes, { id: Date.now().toString(), name: dish }]);
+      setDish('');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>fuck you zion</Text>
+      <Text style={styles.title}>Potluck Dishes</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter a dish..."
+        value={dish}
+        onChangeText={setDish}
+      />
+
+      <Button title="Add Dish" onPress={addDish} />
+
+      <FlatList 
+        data={dishes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text>{item.name}</Text>
+          </View>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +45,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20,
+  },
+  listItem: {
+    width: '100%',
+    padding: 10,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
   },
 });
