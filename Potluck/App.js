4a8bb355df366bce65484ab56be5346008b87223
noah <nkim24@hawk.iit.edu>
2023-10-07 19:56:13 -0500

    testtest

--- Potluck/App.js
+++ Potluck/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>fuck you zion</Text>
+      <Text>noahhh</Text>
       <StatusBar style="auto" />
     </View>
   );
